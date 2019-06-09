d0c35dcdc5419760c97ef5812c951b8b28b44b52
Varvara Babkova <varvara.munday@gmail.com>
2019-06-09 12:45:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -124,7 +124,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
-  programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+     ar = []
+  ar = programmer_hash[:dennis_ritchie][:languages]
+  ar.push("Assembly")
 
 end
